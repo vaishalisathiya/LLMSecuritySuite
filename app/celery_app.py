9854a37aa23fcfe7da43c9462b42395e18bcacea
guardianangel1200 <117119681+guardianangel1200@users.noreply.github.com>
2026-03-26 01:25:31 -0500

    implemented evaluation logic to determine LLM response security based on acceptance criteria

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,9 +1,10 @@
 from celery import Celery
 
 celery_app = Celery(
-    "worker",
+    "app",
     broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    backend="redis://localhost:6379/0",
+    include=["app.tasks"]
 )
 
 celery_app.conf.update(
